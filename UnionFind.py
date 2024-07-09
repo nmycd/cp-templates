c2cd09fa7af5cd2c1769e4fe3418926cf1fd36b6
Eugene Melnyk <melnyk.eugene.o@gmail.com>
2024-07-08 21:52:04 -0400

    Create UnionFind.py

---
+++ UnionFind.py
@@ -0,0 +1,10 @@
+def find(x):
+    if x!=root[x]:
+        root[x] = find(root[x])
+    return root[x]
+
+  def uni(x,y):
+      x,y = find(x),find(y)
+      if x==y: return False
+      root[y]=x
+      return True
