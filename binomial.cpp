9e5fd2e3ac59b0a777678cf162c07b5242847217
Eugene Melnyk <melnyk.eugene.o@gmail.com>
2025-01-25 00:08:16 -0500

    Update binomial.cpp

--- binomial.cpp
+++ binomial.cpp
@@ -7,3 +7,26 @@ for (int n = 1; n <= nums.size(); n++) {
     for (int r = 1; r <= k; r++) 
         ncr[n][r] = (ncr[n - 1][r - 1] + ncr[n - 1][r]) % MOD;
 } 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// https://cp-algorithms.com/algebra/module-inverse.html#mod-inv-all-num
+
+ long long comb(long long a, long long b, long long mod) {
+        if (b > a) return 0;
+        long long numer = 1, denom = 1;
+        for (long long i = 0; i < b; ++i) {
+            numer = numer * (a - i) % mod;
+            denom = denom * (i + 1) % mod;
+        }
+
+        // Fermat's Little Theorem
+        long long denom_inv = 1;
+        long long exp = mod - 2;
+        while (exp) {
+            if (exp % 2) denom_inv = denom_inv * denom % mod;
+            denom = denom * denom % mod;
+            exp /= 2;
+        }
+        return numer * denom_inv % mod;
+    }
