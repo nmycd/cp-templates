6ed07cbb5eee40ef3f637ac03ee13c71ddac4d9f
Eugene Melnyk <melnyk.eugene.o@gmail.com>
2026-06-22 19:49:58 -0400

    Add combinatorial functions and initialization
    Implement combinatorial functions using modular arithmetic

--- binomials_atcoder.cpp
+++ binomials_atcoder.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/modint>
+using namespace std;
+using namespace atcoder;
 
+using mint = modint998244353;
+
+vector<mint> fact, ifact;
+
+void init_comb(int N) {
+    fact.assign(N + 1, 1);
+    ifact.assign(N + 1, 1);
+
+    for (int i = 1; i <= N; i++) {
+        fact[i] = fact[i - 1] * i;
+    }
+
+    ifact[N] = fact[N].inv();
+
+    for (int i = N; i >= 1; i--) {
+        ifact[i - 1] = ifact[i] * i;
+    }
+}
+
+mint C(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n] * ifact[k] * ifact[n - k];
+}
